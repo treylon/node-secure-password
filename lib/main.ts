283907a20e5de2a156703aac37c80d2358d485e0
treylon <treylon@codebrainz.com>
2016-12-20 17:07:56 +0100

    Add export for digest availability checking function

--- lib/main.ts
+++ lib/main.ts
@@ -1,5 +1,6 @@
 import * as encryption from './encryption';
 
+export const isValidDigestAlgorithm = encryption.isValidDigestAlgorithm;
 export const hashPassword = encryption.hashPassword;
 export const comparePasswordWithHash = encryption.comparePasswordWithHash;
 
